33c52d3e21712324d70ec609e3ae1b127353e796
CowBangBoy <s7638569@gmail.com>
2020-04-18 09:03:57 +0800

    first commit

---
+++ precache-manifest.35ffc363788352be899e861a6b18226a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e2712f7f21eeb48b1612173d85df57",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c4401cf7365b57d7d8e",
+    "url": "./static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "b3c8517cd7398fb18d2e",
+    "url": "./static/css/main.0f97b326.chunk.css"
+  },
+  {
+    "revision": "4c4401cf7365b57d7d8e",
+    "url": "./static/js/2.9e6ff1b5.chunk.js"
+  },
+  {
+    "revision": "851eaeb6cb284b98e5dde1961d63308a",
+    "url": "./static/js/2.9e6ff1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c8517cd7398fb18d2e",
+    "url": "./static/js/main.c5e81f2f.chunk.js"
+  },
+  {
+    "revision": "aa77e46ab52b49a4b11d",
+    "url": "./static/js/runtime-main.8e316b4f.js"
+  },
+  {
+    "revision": "2b2994987f98f880a1c54fefa000c38b",
+    "url": "./static/media/photo.2b299498.png"
+  },
+  {
+    "revision": "1aaeb9dd75104b7d59845a7b5bf1ac72",
+    "url": "./static/media/photo_mobile.1aaeb9dd.png"
+  }
+]);
